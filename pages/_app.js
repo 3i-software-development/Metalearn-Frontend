a4d739684ba9b49137e805c2ef575bf8ea67960f
Phu121091 <nguyentrongphuu@gmail.com>
2023-04-03 11:22:24 +0700

    new

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Layout from "@/layout/Layout";
+import { useEffect, useState, useContext } from "react";
+// import { createContext } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+// const AppContext = createContext();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+<Layout><Component {...pageProps} /></Layout>
+  
+      
+
+  )
 }
+
+// export function useApppContext() {
+//   return useContext(AppContext);
+// }
---
+++ views/home/Introduce.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Introduce = () => {
+  return (
+    <div className={style.main}></div>
+  )
+}
+
+export default Introduce
