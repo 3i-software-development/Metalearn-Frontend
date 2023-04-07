0567120714c562af4a1bf7fd2d3f3fbac22c6406
ADMIN\Administrator <nguyentrongphuu@gmail.com>
2023-04-07 18:29:53 +0700

    m

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -9,29 +9,80 @@ import { Autoplay, Navigation } from 'swiper';
 import 'swiper/css';
 import 'swiper/scss/navigation';
 import { IoMdArrowDroprightCircle, IoMdArrowDropleftCircle } from 'react-icons/io';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
-Chart.register(ArcElement);
-const labels = ['section 1', 'section 2', 'section 3', 'section 4', 'section 5', 'section 6'];
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'Doughnut chart',
-    data: [10, 70],
-    backgroundColor: [
-      'blue',
-      'white'
-    ],
-    borderColor: [
-      'rgb(255, 99, 132)',
-      'doggerblue'
-    ],
-    borderWidth: 1,
-    hoverBorderWidth: 1,
-    hoverBorderColor: [
-      'blue',
-      'white'],
-  }]
-};
+// Chart.register(ArcElement);
+// const labels = ['section 1', 'section 2', 'section 3', 'section 4', 'section 5', 'section 6'];
+// const data = {
+//   labels: labels,
+//   datasets: [{
+//     label: 'Doughnut chart',
+//     data: [10, 70],
+//     backgroundColor: [
+//       'blue',
+//       'white'
+//     ],
+//     borderColor: [
+//       'rgb(255, 99, 132)',
+//       'doggerblue'
+//     ],
+//     borderWidth: 1,
+//     hoverBorderWidth: 1,
+//     hoverBorderColor: [
+//       'blue',
+//       'white'],
+//   }]
+// };
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+  // Dữ liệu cho biểu đồ
+  const data = {
+    labels: ['28/3','29/3','30/3','1/4', '2/4', '3/4', '4/4', '5/4'],
+    datasets: [
+      {
+        label: 'Câu hỏi đã làm',
+        data: [70,78,120,100, 119, 54, 211, 60],
+        backgroundColor: 'rgba(75, 192, 192, 0.2)', // Màu nền cho dữ liệu
+        borderColor: 'rgba(75, 192, 192, 1)', // Màu viền cho dữ liệu
+        borderWidth: 1 // Độ dày viền cho dữ liệu
+      },
+      {
+        label: 'Câu làm đúng',
+        data: [12,30,65,80, 76, 53, 188, 30],
+        backgroundColor: 'red', // Màu nền cho dữ liệu
+        borderColor: 'red', // Màu viền cho dữ liệu
+        borderWidth: 1 // Độ dày viền cho dữ liệu
+      }
+    ]
+  };
+
+  // Tùy chọn cho biểu đồ
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        max: 250 // Giá trị tối đa trên trục y
+      }
+    }
+  };
 
 const cx = classNames.bind(styles);
 
@@ -52,6 +103,8 @@ const Banner = ({ img, title, text }) => {
       swiperRef.current.swiper.slideNext();
     }
   };
+
+
 
   return (
     <div className={cx('banner-slide')}>
@@ -83,8 +136,20 @@ const Banner = ({ img, title, text }) => {
         </SwiperSlide>
 
         <SwiperSlide className={cx('item-slide')}>
-          <div className={cx('banner')}>
-            <Image src='/banner/do0.png' alt='banner-metalearn' width='1500' height='1000' className={cx('banner-img','bn2')} />
+
+
+
+   
+
+
+          <div className={cx('banner','banner2')}>
+
+          <Line
+      data={data}
+      options={options}
+    />
+    <h2>Theo dõi và nắm bắt sự tiến bộ của bản thân</h2>
+            {/* <Image src='/banner/do0.png' alt='banner-metalearn' width='1500' height='1000' className={cx('banner-img','bn2')} />
             <div className={cx('text-dia')}>
               <div className={cx('num-quest')}>
                 <div className={cx('count')}>
@@ -143,7 +208,7 @@ const Banner = ({ img, title, text }) => {
                 </div>
               </div>
               <div className={cx('num-quest')}></div>
-            </div>
+            </div> */}
           </div>
         </SwiperSlide>
 
