983ee4d9dcf8f2fe2a45506a83fe77960d72abba
ADMIN\Administrator <ADMIN>
2023-04-19 16:06:29 +0700

    thaianhQuizz

--- components/Quiz/AddForm.js
+++ components/Quiz/AddForm.js
@@ -7,121 +7,97 @@ const cx = classNames.bind(styles);
 export default function AddForm() {
   const [numberQuestions, setNumberQuestions] = useState([]);
   const [tempQuestion, setTempQuestion] = useState(1);
-
-  const quest = [
+  const [checkAnswer, setCheckAnswer] = useState([]);
+  const [questions, setQuestions] = useState([
     {
       id: 1,
-      quest:
-        "Sau khi tổng hợp xong ARN thì mạch gốc của gen có hiện tượng nào sau đây",
+      text: "Khi nào nguyên sinh vật trở thành bào xác?",
+      answers: [
+        { id: 1, text: "Câu trả lời 1", check: true },
+        { id: 2, text: "Câu trả lời 2", check: false },
+        { id: 3, text: "Câu trả lời 3", check: false },
+        { id: 4, text: "Câu trả lời 4", check: false },
+      ],
     },
     {
       id: 2,
-      quest:
-        "Sau khi tổng hợp xong ARN thì mạch gốc của gen có hiện tượng nào sau đây",
+      text: "Khi nào nguyên sinh vật trở thành bào xác?",
+      answers: [
+        { id: 5, text: "Câu trả lời 1", check: true },
+        { id: 6, text: "Câu trả lời 2", check: false },
+        { id: 7, text: "Câu trả lời 3", check: false },
+        { id: 8, text: "Câu trả lời 4", check: false },
+      ],
     },
     {
       id: 3,
-      quest:
-        "Sau khi tổng hợp xong ARN thì mạch gốc của gen có hiện tượng nào sau đây",
+      text: "Khi nào nguyên sinh vật trở thành bào xác?",
+      answers: [
+        { id: 9, text: "Câu trả lời 1", check: true },
+        { id: 10, text: "Câu trả lời 2", check: false },
+        { id: 11, text: "Câu trả lời 3", check: false },
+        { id: 12, text: "Câu trả lời 4", check: false },
+      ],
     },
     {
       id: 4,
-      quest:
-        "Sau khi tổng hợp xong ARN thì mạch gốc của gen có hiện tượng nào sau đây",
-    },
-  ];
-  const anw = [
-    {
-      id: 1,
-      idQuset: 1,
-      ans: "Bị enzim xúc tác phân giải",
-    },
-    {
-      id: 2,
-      idQuset: 1,
-      ans: "Rời nhân để di chuyển ra tế bào chất",
-    },
-    {
-      id: 3,
-      idQuset: 1,
-      ans: "Liên kết với phần tử ARN",
-    },
-    {
-      id: 4,
-      idQuset: 1,
-      ans: "Xoắn lại với mạch bổ sung với nó trên ADN",
+      text: "Khi nào nguyên sinh vật trở thành bào xác?",
+      answers: [
+        { id: 13, text: "Câu trả lời 1", check: true },
+        { id: 14, text: "Câu trả lời 2", check: false },
+        { id: 15, text: "Câu trả lời 3", check: false },
+        { id: 16, text: "Câu trả lời 4", check: false },
+      ],
     },
     {
       id: 5,
-      idQuset: 2,
-      ans: "Bị enzim xúc tác phân giải",
+      text: "Khi nào nguyên sinh vật trở thành bào xác?",
+      answers: [
+        { id: 17, text: "Câu trả lời 1", check: true },
+        { id: 18, text: "Câu trả lời 2", check: false },
+        { id: 19, text: "Câu trả lời 3", check: false },
+        { id: 20, text: "Câu trả lời 4", check: false },
+      ],
     },
     {
       id: 6,
-      idQuset: 2,
-      ans: "Rời nhân để di chuyển ra tế bào chất",
+      text: "Khi nào nguyên sinh vật trở thành bào xác?",
+      answers: [
+        { id: 21, text: "Câu trả lời 1", check: true },
+        { id: 22, text: "Câu trả lời 2", check: false },
+        { id: 23, text: "Câu trả lời 3", check: false },
+        { id: 24, text: "Câu trả lời 4", check: false },
+      ],
     },
     {
       id: 7,
-      idQuset: 2,
-      ans: "Liên kết với phần tử ARN",
+      text: "Khi nào nguyên sinh vật trở thành bào xác?",
+      answers: [
+        { id: 25, text: "Câu trả lời 1", check: true },
+        { id: 26, text: "Câu trả lời 2", check: false },
+        { id: 27, text: "Câu trả lời 3", check: false },
+        { id: 28, text: "Câu trả lời 4", check: false },
+      ],
     },
-    {
-      id: 8,
-      idQuset: 2,
-      ans: "Xoắn lại với mạch bổ sung với nó trên ADN",
-    },
-    {
-      id: 9,
-      idQuset: 3,
-      ans: "Bị enzim xúc tác phân giải",
-    },
-    {
-      id: 10,
-      idQuset: 3,
-      ans: "Rời nhân để di chuyển ra tế bào chất",
-    },
-    {
-      id: 11,
-      idQuset: 3,
-      ans: "Liên kết với phần tử ARN",
-    },
-    {
-      id: 12,
-      idQuset: 3,
-      ans: "Xoắn lại với mạch bổ sung với nó trên ADN",
-    },
-    {
-      id: 10,
-      idQuset: 4,
-      ans: "Bị enzim xúc tác phân giải",
-    },
-    {
-      id: 11,
-      idQuset: 4,
-      ans: "Rời nhân để di chuyển ra tế bào chất",
-    },
-    {
-      id: 12,
-      idQuset: 4,
-      ans: "Liên kết với phần tử ARN",
-    },
-    {
-      id: 13,
-      idQuset: 44,
-      ans: "Xoắn lại với mạch bổ sung với nó trên ADN",
-    },
-  ];
+  ]);
+  function handleAnswerClick(event) {
+    const questionId = parseInt(event.target.dataset.questionId);
+    const answerId = parseInt(event.target.dataset.answerId);
 
-  const [question, setQuestion] = useState(quest);
-  const [answer, setAnswer] = useState(anw);
+    setSelectedAnswers((prevSelectedAnswers) => ({
+      ...prevSelectedAnswers,
+      [questionId]: answerId,
+    }));
+  }
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
+  console.log(selectedAnswers);
+  const [question, setQuestion] = useState(questions);
   const handleNext = () => {
     setTempQuestion(tempQuestion + 1);
   };
   const handleSelect = (value) => {
     setTempQuestion(value);
   };
-  console.log(tempQuestion);
 
   useEffect(() => {
     let array = [];
@@ -130,6 +106,34 @@ export default function AddForm() {
     }
     setNumberQuestions([...array]);
   }, []);
+  function handleAnswerClick(event) {
+    const questionId = parseInt(event.target.dataset.questionId);
+    const answerId = parseInt(event.target.dataset.answerId);
+
+    setSelectedAnswers((prevSelectedAnswers) => ({
+      ...prevSelectedAnswers,
+      [questionId]: answerId,
+    }));
+  }
+  const handleTest = (tempQuestion) => {
+    if (Object.keys(selectedAnswers).includes(tempQuestion.toString())) {
+      const RQuest = question.find((value) => {
+        return value.id === tempQuestion;
+      });
+      const RAnswer = RQuest.answers.find((value) => {
+        return value.check === true;
+      });
+      if (selectedAnswers[tempQuestion.toString()] === RAnswer.id) {
+        setCheckAnswer([...checkAnswer, tempQuestion]);
+      } else {
+        const index = checkAnswer.indexOf(tempQuestion);
+        setCheckAnswer((prev) => {
+          return [...prev.slice(0, index), ...prev.slice(index + 1)];
+        });
+      }
+    }
+  };
+  console.log(checkAnswer);
   return (
     <div className={cx("background")}>
       <div className={cx("tittle")}>
@@ -137,7 +141,6 @@ export default function AddForm() {
       </div>
       <div className={cx("containers")}>
         <div className={cx("quiz")}>
-          {/* <div className={cx("time")}>sssssssssssssssss</div> */}
           <div className={cx("list-questions")}>
             {question.map((value) => (
               <>
@@ -148,24 +151,43 @@ export default function AddForm() {
                     </p>
                   </div>
                   <div className={cx("quizz-left")}>
-                    <h3>{value.quest}</h3>
-                    {answer.map((check) => {
-                      if (check.idQuset === value.id) {
-                        return (
-                          <>
-                            <p>
-                              <span>A.</span>
-                              {check.ans}
-                            </p>
-                          </>
-                        );
-                      }
+                    <h3>{value.text}</h3>
+                    {value.answers.map((answer) => {
+                      return (
+                        <p
+                          key={answer.id}
+                          className={
+                            selectedAnswers[value.id] === answer.id
+                              ? cx("isActive")
+                              : ""
+                          }
+                          data-question-id={value.id}
+                          data-answer-id={answer.id}
+                          onClick={handleAnswerClick}
+                        >
+                          {answer.text}
+                        </p>
+                      );
                     })}
+                    {checkAnswer.includes(value.id) &&
+                    Object.keys(selectedAnswers).includes(
+                      value.id.toString()
+                    ) ? (
+                      <> True </>
+                    ) : Object.keys(selectedAnswers).includes(
+                        value.id.toString()
+                      ) ? (
+                      <>False</>
+                    ) : (
+                      <></>
+                    )}
                     <div className={cx("recommend")}>
                       <a>
                         <i class="fa-brands fa-react fa-beat"></i>
                       </a>
-                      <button>Kiểm tra</button>
+                      <button onClick={() => handleTest(value.id)}>
+                        Kiểm tra
+                      </button>
                       <a>
                         <i class="fa-solid fa-flower"></i>GPT
                       </a>
@@ -192,6 +214,18 @@ export default function AddForm() {
                     </li>
                   );
                 } else {
+                  if (Object.keys(selectedAnswers).includes(value.toString())) {
+                    return (
+                      <li className={cx("done")}>
+                        <a
+                          href={`#${value}`}
+                          onClick={() => handleSelect(value)}
+                        >
+                          {value}
+                        </a>
+                      </li>
+                    );
+                  }
                   return (
                     <li>
                       <a href={`#${value}`} onClick={() => handleSelect(value)}>
