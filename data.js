03a9da32d9e1099b0cdcb9b03babc5994a7b28c4
ZoeTrinh <thuminh10011997@gmail.com>
2023-05-12 10:20:26 +0700

    add lesson

--- data.js
+++ data.js
@@ -28,5 +28,38 @@ export const ArrayData = [
         point: 8,
         munites:"15 munites"
     },
-
 ];
+
+export const selectStatus = [
+    { 
+        label: "NONE",
+        value: ""
+    },
+    { 
+        label: "CREATED",
+        value: "Mới tạo"
+    },
+    { 
+        label: "ACTIVE",
+        value: "Đã kích hoạt"
+    }, 
+    { 
+        label: "DEACTIVE",
+        value: "Ngừng kích hoạt"
+    }, 
+    { 
+        label: "COMPLETE",
+        value: "Hoàn thành"
+    }
+];
+
+export const initialCourseItem = {
+    LessonCode: "",
+    LessonName: "",
+    Status: "",
+    Image: ""
+}
+
+export const initialLessonItem = {
+
+}
