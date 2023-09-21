3c78bf1012bd4621bc46f87ca517f24f52a6fb5d
Jake Hu <hotranphihung@gmail.com>
2023-09-21 09:53:10 +0700

    payment

---
+++ pages/payment/index.js
@@ -0,0 +1,12 @@
+import Payment from '@/components/Payment/Payment';
+import React from 'react'
+import Section from '@/components/Section/Section';
+const index = () => {
+  return (
+    <Section>
+        <Payment />
+    </Section>
+  )
+}
+
+export default index
