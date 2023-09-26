ceacfe13fe8601240c0f87767dd1bc480533bf38
Jake Hu <hotranphihung@gmail.com>
2023-09-26 14:08:08 +0700

    r

--- components/Blog/BlogContent.js
+++ components/Blog/BlogContent.js
@@ -4,11 +4,13 @@ import styles from './style.module.scss';
 
 const cx = classNames.bind(styles);
 
-const BlogContent = ({ content, item }) => {
+const BlogContent = ({ item }) => {
     return (
         <div className={cx('blog-content')}>
-            <h1>{item.title}</h1>
-            <p>{content}</p>
+            <h1 className={cx('blog-title')}>{item.label}</h1>
+            <p className={cx('blog-date')}>{item.date}</p>
+            <p className={cx('blog-text')}>{item.content}</p>
+            <p className={cx('blog-author')}>{item.author}</p>
         </div>
     )
 }
