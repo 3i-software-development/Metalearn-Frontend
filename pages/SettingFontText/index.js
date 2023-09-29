ce8484d2f23898d576749da270276a5709e6c422
Lý Hoài Lâm <hoailam03999@gmail.com>
2023-09-29 11:49:00 +0700

    SettingFontText

---
+++ pages/SettingFontText/index.js
@@ -0,0 +1,9 @@
+import SettingFontText from '../../components/SettingFontText/SettingFontText';
+import Setting from '../../components/SettingFontText/SettingFontText'
+function Index() {
+    return (
+        <div><SettingFontText /></div>
+    );
+}
+
+export default Index;
