faae4e1a59f5e0479c827533ea066a65e891b02f
Jake Hu <hotranphihung@gmail.com>
2023-09-29 10:31:01 +0700

    s

--- components/Collection/CollectionItem.js
+++ components/Collection/CollectionItem.js
@@ -5,36 +5,18 @@ import Image from "next/image";
 
 const cx = classNames.bind(style);
 
-const CollectionItem = ({ data }) => {
+const CollectionItem = ({ item, handleSelectItem }) => {
+    const handleClick = (item) => {
+        handleSelectItem(item);
+    }
     return (
         <div style={{marginBottom: '10px'}}>
             <div style={{marginLeft: '10px'}}>
                 <div style={{display: 'flex'}}>
                     <p><i className="fa-solid fa-thumbtack"></i></p>
-                    <p style={{marginLeft: '5px'}}>Câu hỏi Quiz [{data.total_quiz}]</p>
+                    <p style={{marginLeft: '5px'}}>{item.type} [{item.total}]</p>
                 </div>
-                <div><Image src={data.image} alt="quiz image" width={400} height={200} /></div>
-            </div>
-            <div style={{marginLeft: '10px'}}>
-                <div style={{display: 'flex'}}>
-                    <p><i className="fa-solid fa-thumbtack"></i></p>
-                    <p style={{marginLeft: '5px'}}>Đề thi [{data.total_exam}]</p>
-                </div>
-                <div><Image src={data.image} alt="quiz image" width={400} height={200} /></div>
-            </div>
-            <div style={{marginLeft: '10px'}}>
-                <div style={{display: 'flex'}}>
-                    <p><i className="fa-solid fa-thumbtack"></i></p>
-                    <p style={{marginLeft: '5px'}}>Câu hỏi và câu trả lời [{data.total_question}]</p>
-                </div>
-                <div><Image src={data.image} alt="quiz image" width={400} height={200} /></div>
-            </div>
-            <div style={{marginLeft: '10px'}}>
-                <div style={{display: 'flex'}}>
-                    <p><i className="fa-solid fa-thumbtack"></i></p>
-                    <p style={{marginLeft: '5px'}}>File [{data.total_file}]</p>
-                </div>
-                <div><Image src={data.image} alt="quiz image" width={400} height={200} /></div>
+                <div onClick={() => handleClick(item)}><Image src={item.image} alt="quiz image" width={400} height={200} /></div>
             </div>
         </div>
     );
