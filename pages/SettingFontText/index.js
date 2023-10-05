cb65c5c0d38d83cbaf5e1d62571f3405287b9ea1
Jake Hu <hotranphihung@gmail.com>
2023-10-05 16:21:20 +0700

    s

--- pages/SettingFontText/index.js
+++ pages/SettingFontText/index.js
@@ -1,5 +1,4 @@
 import SettingFontText from '../../components/SettingFontText/SettingFontText';
-import Setting from '../../components/SettingFontText/SettingFontText'
 function Index() {
     return (
         <div><SettingFontText /></div>
