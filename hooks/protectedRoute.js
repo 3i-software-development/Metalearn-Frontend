a99d7fe3b87f0d3c8f61e8ab05b4a35041704fdf
Jake Hu <hotranphihung@gmail.com>
2023-10-14 16:45:27 +0700

    s

--- hooks/protectedRoute.js
+++ hooks/protectedRoute.js
@@ -8,13 +8,13 @@ import Loading from "@/pages/auth/pageLoader";
 const ProtectedRoute = ({ children }) => {
   const router = useRouter();
   const { isAuthenticated, loading } = useAuth();
-    if (loading) {
-        return (
-            <div>
-                <Loading />
-            </div>
-        )
-        }
+    //if (loading) {
+        //return (
+            //<div>
+                //<Loading />
+            //</div>
+        //)
+        //}
     if (!isAuthenticated && (router.pathname !== "/auth/signup" && router.pathname !== "/")) {
         return (
             <Login />
