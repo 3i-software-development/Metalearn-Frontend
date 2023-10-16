a32647b8665d71ce6bccf44aec6890d9cea12b4e
Jake Hu <hotranphihung@gmail.com>
2023-10-16 10:16:29 +0700

    s

--- pages/payment/index.js
+++ pages/payment/index.js
@@ -1,10 +1,12 @@
 import Payment from '@/components/Payment/Payment';
 import React from 'react'
 import Section from '@/components/Section/Section';
+import Pay from '@/components/Payment/Pay';
 const index = () => {
   return (
     <Section>
-        <Payment />
+      <Pay />
+      <Payment />
     </Section>
   )
 }
