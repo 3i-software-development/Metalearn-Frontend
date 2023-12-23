5d75dbd83eb3210dea0abbb5490cfbb0e47ef587
tuannmhe <TuanNMHE172118@fpt.edu.vn>
2023-12-23 10:14:27 +0700

    post news

--- components/Class/Bulletinboard.js
+++ components/Class/Bulletinboard.js
@@ -1,46 +1,30 @@
-import { Avatar, Card, Col, Row } from 'antd';
+import { Avatar, Card, Col, Row } from "antd";
 
 const { Meta } = Card;
 
-const Bulletinboard = () => {
-    return (
-        <Row gutter={16}>
-            <Col span={8}>
-                <Card
-                    style={{ width: 325 }}
-                >
-                    <Meta
-                        avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
-                        title="Card title"
-                        description="This is the description"
-                    />
-                </Card>
-            </Col>
-            <Col span={8}>
-                <Card
-                    style={{ width: 325 }}
-                >
-                    <Meta
-                        avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
-                        title="Card title"
-                        description="This is the description"
-                    />
-                </Card>
-            </Col>
-            <Col span={8}>
-                <Card
-                    style={{ width: 325 }}
-                >
-                    <Meta
-                        avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
-                        title="Card title"
-                        description="This is the description"
-                    />
-                </Card>
-            </Col>
-        </Row>
+const Bulletinboard = ({ data }) => {
+    console.log(data?.Object);
+  return (
+    <Row gutter={16}>
+      {data?.Object?.map((item, i) => {
+        return (
+          <Col key={i} span={8}>
+            <Card style={{ width: 325, marginBottom: '20px' }}>
+              <Meta
+                avatar={
+                  <Avatar src="https://png.pngtree.com/png-vector/20210224/ourmid/pngtree-pin-png-image_2944837.jpg" />
+                }
+                title={item?.Title}
+                // description="This is the description"
+              />
+              <p style={{fontSize: '11px', marginTop: '10px'}}>Người đăng: {item?.CreatedBy}</p>
+              <p style={{fontSize: '11px', marginTop: '10px'}}>Thời gian: {item?.DatePost}</p>
+            </Card>
+          </Col>
+        );
+      })}
+    </Row>
+  );
+};
 
-    )
-}
-
-export default Bulletinboard
+export default Bulletinboard;
--- components/LmsClassForum/index.js
+++ components/LmsClassForum/index.js
@@ -4,27 +4,59 @@ import classNames from "classnames/bind";
 import styles from "./style.module.scss";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import CreateIcon from '@mui/icons-material/Create';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import ModelTrainingIcon from '@mui/icons-material/ModelTraining';
-import ReceiptIcon from '@mui/icons-material/Receipt';
-import PeopleIcon from '@mui/icons-material/People';
-import SubjectIcon from '@mui/icons-material/Subject';
-import { Input, Modal } from 'antd';
+import CreateIcon from "@mui/icons-material/Create";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import ModelTrainingIcon from "@mui/icons-material/ModelTraining";
+import ReceiptIcon from "@mui/icons-material/Receipt";
+import PeopleIcon from "@mui/icons-material/People";
+import SubjectIcon from "@mui/icons-material/Subject";
+import { Input, Modal } from "antd";
 import Bulletinboard from "../Class/Bulletinboard";
+import { useGetCmsItemClassQuery } from "@/lib/Midleware/CmsClassQuery";
+// import { useInsertQuizRefJsonMutation } from "@/lib/Midleware/QuizQuery";
 
 const { TextArea } = Input;
 
 const cx = classNames.bind(styles);
 
 const LmsClassForum = () => {
-  const router = useRouter()
-
+  const [textAreaValue, setTextAreaValue] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+  console.log();
+  // const [insertQuizRefJson, { isLoading, isError, data: dataQ }] = useInsertQuizRefJsonMutation();
+  const { data: ItemClass } = useGetCmsItemClassQuery({
+    classCode: "4u7",
+    userName: "admin",
+  });
+
   const showModal = () => {
     setIsModalOpen(true);
   };
-  const handleOk = () => {
+  const handleOk = async () => {
+    try {
+      const res = await fetch(
+        "https://admin.metalearn.vn/MobileLogin/insertQuizRefJson",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            classCode: "4u7",
+            CreatedBy: "admin",
+            isNews: true,
+            RefContent: textAreaValue,
+            UserCreateRef: "admin",
+          }),
+        }
+      );
+      const data = await res.json();
+      alert(data?.Title);
+      // window.location.reload()
+    } catch (error) {
+      console.log(error);
+    }
     setIsModalOpen(false);
   };
   const handleCancel = () => {
@@ -33,69 +65,82 @@ const LmsClassForum = () => {
 
   const data = [
     {
-      label: 'Tạo tin tức',
-      key: 'news',
+      label: "Tạo tin tức",
+      key: "news",
       icon: <CreateIcon className={cx("icon")} />,
-      modal: true
+      modal: true,
     },
     {
-      label: 'Lịch học',
-      key: 'calendar',
+      label: "Lịch học",
+      key: "calendar",
       icon: <CalendarMonthIcon className={cx("icon")} />,
-
     },
     {
-      label: 'Rèn luyện',
-      key: 'news',
+      label: "Rèn luyện",
+      key: "news",
       icon: <ModelTrainingIcon className={cx("icon")} />,
-
     },
     {
-      label: 'Kết quả',
-      key: 'news',
+      label: "Kết quả",
+      key: "news",
       icon: <ReceiptIcon className={cx("icon")} />,
-
     },
     {
-      label: 'Học viên',
-      key: 'students',
+      label: "Học viên",
+      key: "students",
       classCode: router.query.classCode,
       icon: <PeopleIcon className={cx("icon")} />,
-
     },
     {
-      label: 'Môn học',
-      key: 'news',
+      label: "Môn học",
+      key: "news",
       icon: <SubjectIcon className={cx("icon")} />,
     },
-  ]
+  ];
 
   return (
     <Section>
       <div className={cx("class")}>
         {data?.map((element, i) => {
           return (
-            <Link href={`/my-class?page=lmsClassForum&key=${element?.key}&classCode=${element.classCode}`}>
-              <div key={i} className={cx("item")}>
-                {element.modal ? <div onClick={showModal} className={cx("inner-item")}>
-                  <span>{element?.icon}</span>
-                  <span>{element?.label}</span>
-                </div> : <div className={cx("inner-item")}>
-                  <span>{element?.icon}</span>
-                  <span>{element?.label}</span>
-                </div>}
+            <Link
+              key={i}
+              href={`/my-class?page=lmsClassForum&key=${element?.key}&classCode=${element.classCode}`}
+            >
+              <div className={cx("item")}>
+                {element.modal ? (
+                  <div onClick={showModal} className={cx("inner-item")}>
+                    <span>{element?.icon}</span>
+                    <span>{element?.label}</span>
+                  </div>
+                ) : (
+                  <div className={cx("inner-item")}>
+                    <span>{element?.icon}</span>
+                    <span>{element?.label}</span>
+                  </div>
+                )}
               </div>
             </Link>
           );
         })}
-
       </div>
       <div className={cx("buletinClass")}>
         <h4>Bảng tin lớp học</h4>
-        <Bulletinboard />
+        <Bulletinboard data={ItemClass} />
       </div>
-      <Modal title="Tạo tin tức" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <TextArea rows={7} placeholder="Insert text here" maxLength={10} />
+
+      <Modal
+        title="Tạo tin tức"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <TextArea
+          onChange={(e) => setTextAreaValue(e.target.value)}
+          rows={7}
+          placeholder="Insert text here"
+          maxLength={10}
+        />
       </Modal>
     </Section>
   );
