79bc6191ce510acfe8e04dd687b907a9a98660c4
tieptn <tatiep179@gmail.com>
2024-01-11 11:40:56 +0700

    fix

--- components/Payment/PaymentCallBack.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PaymentCallBack = () => {
-  return (
-    <div>Your payment was successful!</div>
-  )
-}
-
-export default PaymentCallBack
---
+++ pages/payment-callback/vnpay/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>VnPay</div>
+  )
+}
+
+export default index
