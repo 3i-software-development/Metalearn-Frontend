db94b3463f99e04510948e5c51aabea4526b2ca9
Jake Hu <hotranphihung@gmail.com>
2024-01-11 15:28:26 +0700

    s

---
+++ components/PaymentCallBack/PaymentCallBackComponent.js
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+import classNames from "classnames/bind";
+import styles from "./style.module.scss"; // Import SCSS styles for classNames
+import Image from 'next/image'; // Import the next/image component
+
+// Import the SCSS styles using classNames/bind
+
+const cx = classNames.bind(styles);
+
+export default function PaymentCallBackComponent({ router }) {
+    const { query, query: { paymentProvider } } = router;
+    const { vnp_Amount, vnp_BankCode, vnp_BankTranNo, vnp_CardType, vnp_OrderInfo, vnp_PayDate, vnp_ResponseCode, vnp_TmnCode, vnp_TransactionNo, vnp_TxnRef, vnp_SecureHash } = query;
+    const formattedAmount = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(vnp_Amount);
+
+
+    return (
+        <div className={cx('container-page')}>
+            <div className={cx('container')}>
+                <Image className={cx('icon')} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk2AgB3oBS5MQf4r5nnmeITwN-dKaByDlARw&usqp=CAU" alt="checked" width={100} height={100} />
+                <div className={cx('payment-sucess')}>You have completed your payment</div>
+                <div className={cx('payment-provider')}>Method used {paymentProvider}</div>
+                <div className={cx('vnp_Amount')}>Amount {formattedAmount}</div>
+                <div className={cx('bank-info')}>
+                    <div className={cx('vnp_BankCode')}>BankCode {vnp_BankCode}</div>
+                    <div className={cx('vnp_BankTranNo')}>BankTranNo {vnp_BankTranNo}</div>
+                </div>
+                <div>
+                    <div className={cx('vnp_CardType')}><span style={{ fontWeight: 'bold' }}>Cardtype</span> {vnp_CardType}</div>
+                    <div className={cx('vnp_OrderInfo')}><span style={{ fontWeight: 'bold' }}>OrderInfo </span> {vnp_OrderInfo}</div>
+                    <div className={cx('vnp_PayDate')}><span style={{ fontWeight: 'bold' }}>PayDate </span> {vnp_PayDate}</div>
+                    <div className={cx('vnp_ResponseCode')}><span style={{ fontWeight: 'bold' }}>ResponseCode </span> {vnp_ResponseCode}</div>
+                    <div className={cx('vnp_TmnCode')}><span style={{ fontWeight: 'bold' }}>TmnCode </span> {vnp_TmnCode}</div>
+                    <div className={cx('vnp_TransactionNo')}><span style={{ fontWeight: 'bold' }}>TransactionNo </span> {vnp_TransactionNo}</div>
+                    <div className={cx('vnp_TxnRef')}><span style={{ fontWeight: 'bold' }}>TxnRef </span> {vnp_TxnRef}</div>
+                </div>
+                <div className={cx('vnp_SecureHash')} style={{ wordWrap: 'break-word' }}>
+                    <span style={{ fontWeight: 'bold' }}>SecureHash</span> {vnp_SecureHash}
+                </div>
+            </div>
+        </div>
+    );
+}
--- pages/payment-callback/[paymentProvider].js
+++ pages/payment-callback/[paymentProvider].js
@@ -1,25 +1,14 @@
 import { useRouter } from 'next/router'
+import PaymentCallBackComponent from '@/components/PaymentCallBack/paymentCallBackComponent';
 
 export default function Page() {
     const router = useRouter()
-    const { query, query: { paymentProvider } } = router;
-    const { vnp_Amount, vnp_BankCode, vnp_BankTranNo, vnp_CardType, vnp_OrderInfo, vnp_PayDate, vnp_ResponseCode, vnp_TmnCode, vnp_TransactionNo, vnp_TxnRef, vnp_SecureHash } = query;
+    // const { query, query: { paymentProvider } } = router;
+    // const { vnp_Amount, vnp_BankCode, vnp_BankTranNo, vnp_CardType, vnp_OrderInfo, vnp_PayDate, vnp_ResponseCode, vnp_TmnCode, vnp_TransactionNo, vnp_TxnRef, vnp_SecureHash } = query;
 
     return (
         <>
-            <h1>Payment Callback for</h1>
-            <h2>{paymentProvider}</h2>
-            <p>Amount: {vnp_Amount}</p>
-            <p>Bank Code: {vnp_BankCode}</p>
-            <p>Bank Transaction No: {vnp_BankTranNo}</p>
-            <p>Card Type: {vnp_CardType}</p>
-            <p>Order Info: {vnp_OrderInfo}</p>
-            <p>Pay Date: {vnp_PayDate}</p>
-            <p>Response Code: {vnp_ResponseCode}</p>
-            <p>Tmn Code: {vnp_TmnCode}</p>
-            <p>Transaction No: {vnp_TransactionNo}</p>
-            <p>Txn Ref: {vnp_TxnRef}</p>
-            <p>Secure Hash: {vnp_SecureHash}</p>
+            <PaymentCallBackComponent router={router} />
         </>
     )
 }
